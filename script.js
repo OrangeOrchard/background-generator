dba4ca3c0a06bb8c4338a50225e541aa3d083d9a
unknown <lancebraham001@yahoo.com>
2025-03-09 17:01:04 -0400

    added more files

---
+++ script.js
@@ -0,0 +1,19 @@
+const line = document.getElementById("text");
+const textField = document.getElementById("field");
+const button = document.querySelector("button");
+
+function sampleFunction1() {
+	const value = textField.value;
+	if(value.length > 2){
+		line.textContent = value;
+	}
+	console.log(value);
+}
+
+button.addEventListener("click", sampleFunction1);
+
+textField.addEventListener("keydown", function(e) {
+	if(e.code == "Enter") {
+		sampleFunction1();
+	}
+});
